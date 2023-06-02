e9a8626f7ef723a4480e9655509734456e7df432
guilhiz <guilhermevilelasobral@gmail.com>
2023-06-02 17:14:25 -0300

    feat: add signUp page template

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,13 +1,17 @@
-'use client';
-
 import Link from 'next/link';
-import { GithubLogo, GoogleLogo } from '@phosphor-icons/react';
 
-const SignIn = () => {
+const SignUp = () => {
   return (
     <section className="flex flex-col justify-center items-center h-screen">
-      <h1 className="text-primary focus:text-primary-focus text-5xl font-bold mb-5">Login</h1>
+      <h1 className="text-primary focus:text-primary-focus text-5xl font-bold mb-5">Cadastro</h1>
       <form className="form-control justify-center w-full max-w-md px-4 py-4">
+        <label className="label">
+          <span className="label-text text-xl font-medium">Nome</span>
+        </label>
+        <input type="text" placeholder="Digete seu nome" className="input input-bordered w-full " />
+        <label className="label">
+          <span className="label-text-alt text-error text-base">No mínimo 5 caracteres</span>
+        </label>
         <label className="label">
           <span className="label-text text-xl font-medium">Email</span>
         </label>
@@ -25,22 +29,13 @@ const SignIn = () => {
         <button className="btn btn-primary font-bold text-xl">Submit</button>
       </form>
       <p className="mt-2 text-base">
-        Novo no Smart Devs?
-        <Link href="/cadastro" className="link text-info hover:text-opacity-75 ml-2">
-          Crie sua conta aqui.
+        Já tem uma conta?
+        <Link href="/" className="link text-info hover:text-opacity-75 ml-2">
+          Faça login aqui.
         </Link>
       </p>
-      <div className="divider">Ou continuar com</div>
-      <div className="flex w-full max-w-md justify-around">
-        <button className="btn btn-neutral  w-5/12">
-          <GithubLogo size={22} color="#cccccc" /> Github
-        </button>
-        <button className="btn btn-neutral w-5/12">
-          <GoogleLogo size={22} color="#cccccc" /> Google
-        </button>
-      </div>
     </section>
   );
 };
 
-export default SignIn;
+export default SignUp;
